fb002bd8fa3350b7e6c25970dbadfbd70df1eee7
Mehfooz-ur-Rehman <68416499+MehfoozurRehman@users.noreply.github.com>
2023-09-19 00:19:59 +0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,5 @@
 import App from "./App";
-import ReactDOM from "react-dom";
+import React from "react";
+import { createRoot } from "react-dom/client";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root"),
-);
+createRoot(document.getElementById("root")).render(<App />);
