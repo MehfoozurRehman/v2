cae2efbc17380791bae8ac7c06ecc056dcb02766
mehfoozurrehman <mehfoozijaz786@gmail.com>
2024-01-14 23:10:40 +0500

    Update Portfolio.js

--- src/components/main/portfolio/Portfolio.js
+++ src/components/main/portfolio/Portfolio.js
@@ -1,4 +1,5 @@
 import "./Portfolio.scss";
+
 import img_project_1 from "../../../assets/project-pic/to-do-list.webp";
 import img_project_2 from "../../../assets/project-pic/codepen.webp";
 import img_project_3 from "../../../assets/project-pic/fbmacorp.webp";
